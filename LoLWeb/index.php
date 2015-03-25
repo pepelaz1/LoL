4ede4fbfb8e7da47ed2e6695a5f707f598411a75
filimonov1987 <filimonov1987@list.ru>
2015-03-25 22:43:18 +0300

    Начало работы над главной страницей

---
+++ LoLWeb/index.php
@@ -0,0 +1,11 @@
+<HTML>
+	<TABLE cellspacing="0" cellpadding = "0">
+		<TR>
+		    <TD bgcolor="#000000" width="15%"><img src="Resource/Logo.png"></TD>
+		    <td bgcolor="#000000" width="85%"/>
+		</TR>
+		<TR>
+			<td height="750" width="100%" style="background-image: url(Resource/HomeBackground1.png); background-position:center"></td>
+		</TR>
+	</TABLE>
+</HTML>
