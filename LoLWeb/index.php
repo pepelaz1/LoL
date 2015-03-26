9626f7302de095229023ab24d47441f4c0683ebd
filimonov1987 <filimonov1987@list.ru>
2015-03-26 21:24:02 +0300

    Первоначальная разметка страницы со статистикой

--- LoLWeb/index.php
+++ LoLWeb/index.php
@@ -1,11 +1,34 @@
 <HTML>
-	<TABLE cellspacing="0" cellpadding = "0">
-		<TR>
-		    <TD bgcolor="#000000" width="15%"><img src="Resource/Logo.png"></TD>
-		    <td bgcolor="#000000" width="85%"/>
-		</TR>
-		<TR>
-			<td height="750" width="100%" style="background-image: url(Resource/HomeBackground1.png); background-position:center"></td>
-		</TR>
-	</TABLE>
+<TABLE cellspacing="0" cellpadding="0">
+	<TR>
+		<TD bgcolor="#000000" width="15%"><img src="Resource/Logo.png"></TD>
+		<td bgcolor="#000000" width="85%" />
+	</TR>
+	<TR>
+		<td height="750" width="100%"
+			style="background-image: url(Resource/HomeBackground1.png); background-position: center">
+
+			<form action="search.php" method="get">
+				<p>
+					<div align="center">
+						<select required>
+							<option selected value="NA">North America</option>
+							<option value="EUW">Europe West</option>
+							<option value="EUNE">Europe Nordic && East</option>
+							<option value="BR">Brazil</option>
+							<option value="TR">Turkey</option>
+							<option value="RU">Russia</option>
+							<option value="LAN">Latin Ameria North</option>
+							<option value="LAS">Latin Ameria South</option>
+							<option value="OCE">Oceania</option>
+						</select>
+						<input type="text" size="20" name="summonerName" placeholder="Summoner Name..."/>
+						<input type="submit" value="Search" />
+					</div>
+				</p>
+			</form>
+
+		</td>
+	</TR>
+</TABLE>
 </HTML>
---
+++ LoLWeb/search.php
@@ -0,0 +1,64 @@
+<HTML>
+<TABLE cellspacing="0" cellpadding="0" width="100%">
+	<TR>
+		<TD bgcolor="#000000" width="15%"><img src="Resource/Logo.png"></TD>
+		<td bgcolor="#000000" width="85%" />
+	</TR>
+</TABLE>
+<table width="100%">
+	<tr>
+		<td width="5%" align="left">Home</td>
+		<td width="5%" align="left">Summoner</td>
+		<td width="5%" align="left">Game</td>
+		<td width="85%" align="right">Summoner:</td>
+	</TR>
+</table>
+<table width="100%">
+	<tr>
+		<td align="left">Top five champions</td>
+	</TR>
+</table>
+<table cellspacing="1" cellpadding="1" width="100%" height="20%">
+	<tr>
+		<td align="center">Champion1</td>
+		<td align="center">Champion2</td>
+		<td align="center">Champion3</td>
+		<td align="center">Champion4</td>
+		<td align="center">Champion5</td>
+	</TR>
+</table>
+<table width="100%">
+	<tr>
+		<td align="left" width="50%">Ranked Ratings</td>
+		<td align="left" width="50%">Statistics</td>
+	</TR>
+</table>
+<table cellspacing="1" cellpadding="1" width="100%" height="40%">
+	<tr>
+		<td width="50%">
+			<table width="100%" height="100%">
+				<tr>
+					<td style="vertical-align: top; text-align: center">Team 3v3</td>
+					<td style="vertical-align: top; text-align: center">Solo 5v5</td>
+					<td style="vertical-align: top; text-align: center">Team 5v5</td>
+				</tr>
+			</table>
+		</td>
+		<TD width="50%">
+			<table width="100%" height="100%">
+				<tr height="10%">
+					<td align="center">Ranked 5v5:</td>
+					<td align="center">Normal 5v5:</td>
+					<td align="center">Ward score:</td>
+				</tr>
+				<tr>
+					<td width="30%"></td>
+					<td width="30%"></td>
+					<td width="30%"></td>
+				</tr>
+			</table>
+		</TD>
+	</tr>
+
+</table>
+</HTML>
